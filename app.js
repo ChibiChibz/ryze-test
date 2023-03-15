c920a333c94aae85c36567fd1750fe08da96d44a
Chi Thanh Pham <chithanh.pham@syzygy.de>
2023-03-15 23:51:54 +0100

    chore: init draft

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+
+let burger = document.getElementById('burger');
+let openMenu = document.getElementById('menu-open');
+let closeMenu = document.getElementById('menu-close');
+let navLinks = document.getElementById('nav-links');
+
+console.log(burger);
+
+burger.addEventListener('click', function() {
+    console.log('clicked');
+    openMenu.classList.toggle('active');
+    closeMenu.classList.toggle('active');
+    navLinks.classList.toggle('active');
+    document.body.classList.toggle('disable-scroll');
+
+});
+
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 959) {
+        openMenu.classList.remove('active');
+        openMenu.classList.add('active');
+        closeMenu.classList.remove('active');
+        navLinks.classList.remove('active');
+        document.body.classList.remove('disable-scroll');
+    }
+});
